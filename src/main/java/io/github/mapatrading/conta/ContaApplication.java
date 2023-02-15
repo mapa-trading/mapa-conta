5582b0facfd78d87784b89abe8b38a91b25f4ba1
Arlleymatheus <arlleymatheus10@hotmail.com>
2023-02-15 20:56:05 -0300

    chore: update entity

--- src/main/java/io/github/mapatrading/conta/ContaApplication.java
+++ src/main/java/io/github/mapatrading/conta/ContaApplication.java
@@ -2,7 +2,9 @@ package io.github.mapatrading.conta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ContaApplication {
 
